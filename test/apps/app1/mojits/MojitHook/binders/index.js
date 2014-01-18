f563285338078796024744de2e951df8a56fa5a3
aljimenez <albert.louis.jimenez@gmail.com>
2014-01-17 17:33:32 -0800

    Fixed refreshing of MojitHook, by updating the corresponding hook's viewId after updating the node after an inovke.

--- test/apps/app1/mojits/MojitHook/binders/index.js
+++ test/apps/app1/mojits/MojitHook/binders/index.js
@@ -25,9 +25,22 @@ YUI.add('mojit-hook-binder-index', function (Y, NAME) {
                     }
                 },
                 rpc: refreshOnServer
-            }, function (error, data) {
-                // Make sure mojitHook is rendered
+            }, function (error, data, meta) {
+                if (error) {
+                    Y.Debug.error('mojit-hook', error, 'error', NAME);
+                    return;
+                }
+                // Replacing this node with the new rendered mojit-hook.
                 this.node.replace(data);
+                // This will result in a new binder being created for mojit-hook,
+                // so we need to change the viewId being used by mojit-hook such that
+                // the hook's binder is reflected.
+                // This is done automatically when ac.debug.render is called on a mojit hook,
+                // however since the rendering was done through an invoke, the debugger cannot know
+                // what will be done with the rendered data so it shouldn't try to match any resulting binders
+                // with this mojit hook. In this case we know that there only one binder will result and
+                // it belongs to mojit-hook.
+                Y.Debug.get('mojit-hook')._viewId = meta.binders[Y.Object.keys(meta.binders)[0]].viewId;
             }.bind(this));
         }
     };
