2cd9422aeb19eaed73306862d87e0e687e3bab12
jimenez <jimenez@yahoo-inc.com>
2014-12-04 11:25:35 -0800

    Moved mojito-debug middleware above mojito's middlewares.

--- examples/apps/app1/app.js
+++ examples/apps/app1/app.js
@@ -6,11 +6,11 @@ app.set('port', process.env.PORT || 8666);
 
 libmojito.extend(app);
 
-app.use(libmojito.middleware());
 app.use(require('./node_modules/mojito-debug/middleware/mojito-debug.js')({
     Y: app.mojito.Y,
     store: app.mojito.store
 }));
+app.use(libmojito.middleware());
 app.mojito.attachRoutes();
 
 app.listen(app.get('port'));
